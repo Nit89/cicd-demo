a4bd9e060469a422c971d0e81215a8cf07f392cb
Nit89 <nitesh382singh.com>
2024-09-28 23:54:36 +0530

    node js application created

---
+++ app.js
@@ -0,0 +1,14 @@
+// Simple Node.js application
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+// Listen on port 3000
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
